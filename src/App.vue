8c77d611d5fc04eda1cbd2ad8a786541b107c468
Erick Marroquin <marro.focus@gmail.com>
2022-09-10 12:03:36 -0600

    complete app - testing pending

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <Indesicion/>
+</template>
+
+<script>
+import Indesicion from "./components/Indesicion.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Indesicion
+  }
+}
+</script>
+
+<style>
+html, body {
+  background-color: black;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
